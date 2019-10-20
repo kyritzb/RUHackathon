14e5e8acd10e6815b486e0bb384e240ab6bbd40c
Femi <35737062+AdefemiF@users.noreply.github.com>
2019-10-20 04:55:29 -0400

    MouseClick

---
+++ OnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public InputField inputField;
+    Button button;
+    string letter;
+    void Start() {
+        button = this.GetComponent<Button>();
+        letter = button.GetComponentInChildren<Text>().text;
+        button.onClick.AddListener(TaskOnClick);
+    }
+    public void TaskOnClick() {
+        inputField.text += letter;
+    }
+}
